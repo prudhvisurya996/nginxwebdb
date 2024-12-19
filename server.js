c6441db965b1661542da4ffcd9fbb0b7971894f9
prudhvisurya996 <150457002+prudhvisurya996@users.noreply.github.com>
2024-12-19 12:25:27 +0530

    Update server.js

--- server.js
+++ server.js
@@ -66,8 +66,8 @@ app.post('/register', (req, res) => {
 
 // Login endpoint
 app.post('/login', (req, res) => {  
-  const { username, password } = req.body;  
-  const query = `SELECT * FROM users WHERE username = ? AND password = ?`;  
+  const { email, password } = req.body;  
+  const query = `SELECT * FROM users WHERE email = ? AND password = ?`;  
   db.query(query, [username, password], (err, results) => {  
   if (err || results.length === 0) {  
    res.status(401).send({ success: false, message: 'Invalid credentials!' });  
