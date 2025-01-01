4ea941fefc109b293bb150e61691ea6224307989
prudhvisurya996 <150457002+prudhvisurya996@users.noreply.github.com>
2025-01-02 01:49:46 +0530

    Update sonarinstall.sh

--- sonarinstall.sh
+++ sonarinstall.sh
@@ -2,4 +2,6 @@
 sudo apt update
 sudo apt install openjdk-11-jdk
 docker run --name sonar -d -p 9000:9000 sonarqube:lts
+npx sonarqube-scanner -Dsonar.login=squ_7f53a5ff6a5dda46b3598d4801f98ecd0251a405
+
 
